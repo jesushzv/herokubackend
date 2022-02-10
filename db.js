09eacfa7dc70dd9fd7864db55cf127591489bd57
Jesus Hector Zamora <jhector.zamora@hotmail.com>
2022-02-09 17:34:02 -0800

    second commit

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 module.exports = {
-    database : 'ejercicio',
-    user : 'postgres',
-    password : '123',
-    host : 'localhost',
+    database : 'd5a6da6k294h9b',
+    user : 'qfeesmgauipecb',
+    password : '085805bc3bee7bd78c75428c08aec7f4ef7d0e5a50b38689e8e952986536951d',
+    host : 'ec2-54-235-98-1.compute-1.amazonaws.com',
     dialect : 'postgres'
 }
