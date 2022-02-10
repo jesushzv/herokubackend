b376fe3a67c756ef9de63c469f6e4e9356003c02
Jesus Hector Zamora <jhector.zamora@hotmail.com>
2022-02-09 18:21:22 -0800

    4

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const app = require('express');
 routes = require('./routes/main.js');
 
-routes(app);
-
+app.get('/', (req, res) => {
+    res.send('Welcome to the home page!');
+})
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
